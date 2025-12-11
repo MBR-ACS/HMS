5596413ff2e0a84c5c1ff9828d0f27aacc119c4a
MBR-ACS <bhagyaraju.marapatla@alladicloudsolutions.com>
2025-12-11 18:16:49 +0530

    few updates in css

--- doctors/urls.py
+++ doctors/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from doctors import views
 
 urlpatterns = [
-    path('artho/', views.arthopedic),
+    path('artho/', views.arthopedic, name = 'bhagya'),
     path('cardio/', views.cardiology),
     path('derma/', views.dermatology),
 ]
