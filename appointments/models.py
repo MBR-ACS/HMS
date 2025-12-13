5b68fff54477fddf513bba34d4a9369f8f0574c8
MBR-ACS <bhagyaraju.marapatla@alladicloudsolutions.com>
2025-12-13 18:36:28 +0530

    few updateds in models

--- appointments/models.py
+++ appointments/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 from doctors.models import Doctor
-
+from patients.models import Patient
 # Create your models here.
 class Appointments(models.Model):
     appointment_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, null=False)
-    phone = models.CharField(max_length=10, null=False)
-    city = models.CharField(max_length=50, null=True)
-    notes = models.TextField(null=True)
+    patient_id = models.ForeignKey(Patient, on_delete=models.SET_NULL, null=True)
     doctor_id = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True) # if doctor is deleted this id will be null
-    date = models.DateTimeField()
+    appointment_date = models.DateTimeField()
+    is_open = models.BooleanField(default=True) # appointment is completed or not
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.appointment_date.strftime("%d-%m-%Y %I:%M %p")
