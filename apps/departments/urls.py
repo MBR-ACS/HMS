2cf1fcb5e16be15daf58eb2871c01654d3154859
MBR-ACS <bhagyaraju.marapatla@alladicloudsolutions.com>
2025-12-19 13:37:14 +0530

    added edit delete create views

--- apps/departments/urls.py
+++ apps/departments/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from apps.departments import views
 
 urlpatterns = [
-    path('list_departments/', views.list_departments, name = 'list-departments')
+    path('list_departments/', views.list_departments, name = 'list-departments'),
+    path('create_department/', views.add_department, name='add-dept'),
+    path('edit_department/<int:dept_id>', views.edit_department, name='edit-dept'),
+    path('delete_department/<int:dept_id>', views.delete_department, name='delete-dept'),
 ]
--- apps/departments/views.py
+++ apps/departments/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from apps.departments.models import Department
+from apps.departments.forms import DepartmentForm
 
 # Create your views here.
 def list_departments(request):
     departments = Department.objects.all()
     return render(request, 'departments/list_departments.html', context={'departments':departments})
+
+def add_department(request):
+    if request.method == 'POST':
+        form = DepartmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #return HttpResponse("***Form Submitin successful***")
+            return redirect('list-departments')
+        else:
+            return HttpResponse("***Your form is invaid***")
+    if request.method == 'GET':
+        form = DepartmentForm()
+    return render(request, 'departments\create_department.html', context={'form':form})
+
+def edit_department(request, dept_id):
+    obj = Department.objects.get(dept_id=dept_id)
+    form = DepartmentForm(instance=obj)
+
+    if request.method == 'POST':
+        form = DepartmentForm(request.POST, instance=obj)
+        if form.is_valid():
+            form.save()
+            #return HttpResponse("***Form Submitin successful***")
+            return redirect('list-departments')
+        else:
+            return HttpResponse("***Your form is invaid***")
+    return render(request, 'departments\create_department.html', context={'form':form})
+
+def delete_department(request, dept_id):
+    obj  = Department.objects.get(dept_id=dept_id)
+    obj.delete()
+    return redirect('list-departments')
